add130c511548c60e190aee747bba5dfdf242c81
Daniel-Ferreira-2004 <danikferreira69@gmail.com>
2025-06-25 20:25:56 -0300

    Efeito de aparecer na index

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+window.revelar = ScrollReveal({reset:true});
+
+revelar.reveal(".ComoFunciona", {
+    duration: 2000,
+    distance: "70px",
+    origin: "top"
+});
+
+revelar.reveal(".façaParte", {
+    duration: 2000,
+    distance: "70px",
+    origin: "top"
+});
+
+revelar.reveal(".empresas", {
+    duration: 2000,
+    distance: "70px",
+    origin: "top"
+});
